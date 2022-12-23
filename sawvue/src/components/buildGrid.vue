e0307de670ba0ec54708908395953b3dc8403258
ClaraSantosmf <clarasantosmf@gmail.com>
2022-12-23 16:04:27 -0300

    Versão beta

--- sawvue/src/components/buildGrid.vue
+++ sawvue/src/components/buildGrid.vue
@@ -1,58 +1,83 @@
 <template>
   <table>
     <tr v-for="(line, r) in grid" :key="`${line}-${r}`">
-      <td v-for="(coluna, c) in line" :key="`${r}-${c}-${coluna}`" :class="classes(r, c)">
-      </td>
+      <td
+        v-for="(coluna, c) in line"
+        :key="`${r}-${c}-${coluna}`"
+        :class="classes(r, c)"
+      ></td>
     </tr>
   </table>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        grid: [
-          [0,1,0,0],
-          [0,0,1,0],
-          [0,0,0,1],
-          [1,0,0,0]
-        ],
-        line: 4,
-        columns: 4
-      }
-    },
-    methods:{
-      update(){
-        let new_matriz = Array(this.line).fill(Array(this.columns).fill(0))
-        for (let i=0; i <this.line; i++){
-          for (let y=0; y<this.columns; y++){
-            let alives = this.vizinhos(this.grid, i, y)
-            new_matriz[i][y] = parseInt((this.new_matriz[i][y] === 0 && alives === 3) || (this.grid[i][y] === 1 && !(alives<2 || alives>3 )))
-              return new_matriz
+export default {
+  data() {
+    return {
+      grid: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      lines: 4,
+      columns: 5,
+    };
+  },
+  methods: {
+    update() {
+      let new_matriz = JSON.parse(JSON.stringify(this.grid))
+      for (let i = 0; i < this.lines; i++) {
+        for (let j = 0; j < this.columns; j++) {
+          let alives = this.vizinhos(i, j);
+          if (this.grid[i][j] === 1) {
+            if (alives < 2 || alives > 3) {
+              new_matriz[i][j] = 0
+            }
+          } else {
+            if (alives === 3) {
+              new_matriz[i][j] = 1
             }
           }
-          this.grid.push([])
-          this.grid.pop()
-      },
-      vizinhos(){
-        return
-      },
-      classes(linha, coluna){
-        console.log(this.grid[linha][coluna])
-        return {
-          "pintado": this.grid[linha][coluna]
+
         }
       }
+      this.grid = new_matriz;
+      return new_matriz
     },
-    created(){
-      const acabate = ()=> {
-        this.update()
-        // console.log("aaaa")
-        setTimeout(acabate, 500)
+    vizinhos(x, y) {
+      let somatorio = 0;
+      for (let linha_x of [-1, 0, 1]) {
+        for (let coluna_y of [-1, 0, 1]) {
+          const vizinho_x = x + linha_x;
+          const vizinho_y = y + coluna_y;
+          if (
+            vizinho_x < 0 ||
+            vizinho_y < 0 ||
+            vizinho_x >= this.grid.length ||
+            vizinho_y >= this.grid[0].length) continue
+          if (vizinho_x == x && vizinho_y == y) continue
+          somatorio += this.grid[vizinho_x][vizinho_y]
+        }
       }
-      acabate()
-    }
-  }
+      console.log(`${x} ${y} esse é o somatório ${somatorio}`);
+      return somatorio;
+    },
+    classes(linha, coluna) {
+      console.log(this.grid[linha][coluna]);
+      return {
+        pintado: this.grid[linha][coluna],
+      };
+    },
+  },
+  created() {
+    const acabate = () => {
+      this.update();
+      setTimeout(acabate, 800);
+    };
+    acabate();
+  },
+};
 </script>
 
 <style scoped>
@@ -65,5 +90,4 @@ table td {
 .pintado {
   background-color: black;
 }
-
 </style>
